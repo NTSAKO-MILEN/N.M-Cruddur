1dbcecec9c8d49cf3ce4ece170d46f8b3949bb38
NTSAKO MILEN <ntsakomilen3@gmail.com>
2025-06-01 22:43:37 +0000

    added the X-Ray integration to your main.py file.

---
+++ backend/app.py/main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+  import os
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+app = Flask(__name__)
+
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
+
+@app.route('/')
+def home():
+    return "Hello, Backend!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
+  
